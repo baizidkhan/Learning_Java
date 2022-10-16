5c38be96197d967944e5232d3b06356c158c0d64
baizidkhan <baizidkhan7515@gmail.com>
2022-10-16 12:07:00 +0530

    Initial version

---
+++ src/Variable_01/FirstCode.java
@@ -0,0 +1,13 @@
+package Variable_01;
+
+public class FirstCode {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+//        System.out.println("Hello, World!\n");
+//        System.out.println("Hello Bangladesh!!");
+    }
+}
---
+++ src/Variable_01/Second.java
@@ -0,0 +1,7 @@
+package Variable_01;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Heyy!!");
+    }
+} 
