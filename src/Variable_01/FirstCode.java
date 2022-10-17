c96a0abe8a7ac04caf379dd83f882fb88f86f1c2
baizidkhan <baizidkhan7515@gmail.com>
2022-10-17 23:08:51 +0530

    add some arethmetic operator

--- src/Variable_01/FirstCode.java
+++ src/Variable_01/FirstCode.java
@@ -11,5 +11,15 @@ public class FirstCode {
         byte b=(byte)a;
         System.out.println(a);
         System.out.println(b);
+
+        int num1 = 10, num2 = 20, sum = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // adding num1 and num2
+        sum = num1 + num2;
+        System.out.println("The sum = " + sum);
     }
 }
